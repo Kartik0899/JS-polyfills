20d694bedfd973139cdceaac03689c3a2e7b155c
Kartik0899 <72159742+Kartik0899@users.noreply.github.com>
2023-05-21 14:26:33 +0530

    Add files via upload

---
+++ FilterPolyfil/filterpolyfil.js
@@ -0,0 +1,19 @@
+// filter() - 
+
+// The filter() method creates a new array filled with elements that pass a test provided by a function.
+// The filter() method does not execute the function for empty elements.
+// The filter() method does not change the original array.
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+// const res = arr.filter((item, i, arr) => {
+//     return item > 3;
+// })
+
+// console.log(res);
+
+
+
+
+// Polyfil for filter() - 
+
