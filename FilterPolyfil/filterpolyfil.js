6685310d82e5302cfb102ee18974d6e3e0a14e4d
Kartik0899 <jaybudhraja08@gmail.com>
2023-05-21 14:34:39 +0530

    filter polyfill

--- FilterPolyfil/filterpolyfil.js
+++ FilterPolyfil/filterpolyfil.js
@@ -17,3 +17,19 @@
 
 // Polyfil for filter() - 
 
+Array.prototype.myFilter = function(callback){
+    let temp = [];
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            temp.push(this[i]);
+        }
+    }
+    return temp;
+}
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8];
+const res = arr.myFilter((item, i, arr) => {
+    return item > 5;
+})
+
+console.log(res);
